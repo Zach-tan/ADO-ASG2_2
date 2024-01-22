34f4a9b12288527202263fc95c2f26f814daa4d3
Zach-tan <zthb05@gmail.com>
2024-01-22 20:31:29 +0800

    fixed materialisation of fresh tables

--- models/raw/raw_order_detail.sql
+++ models/raw/raw_order_detail.sql
@@ -4,4 +4,6 @@ SELECT src.*
 FROM {{ source('ADO', 'FRESH_ORDERDETAIL') }} AS src
 LEFT JOIN {{ this }} AS dest
 ON src.ORDERID = dest.ORDERID AND src.PRODUCTID = dest.PRODUCTID
+{% if is_incremental() %}
 WHERE dest.ORDERID IS NULL AND dest.PRODUCTID IS NULL
+{% endif %}
